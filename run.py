1fa31ab19ebe6f922399f0a4970b37cbb6292837
Inc21 <intc21@gmail.com>
2023-03-28 23:57:42 +0000

    Fix GitPod reported errors in the syntax

--- run.py
+++ run.py
@@ -17,8 +17,8 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated 
-    by commas. The loop will repeatedly request data, until it is valid. 
+    via the terminal, which must be a string of 6 numbers separated,
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -26,20 +26,20 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
+
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
             print("Data is valid.")
             break
 
-    return sales_data    
+    return sales_data
 
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int, 
+    Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
     try:
